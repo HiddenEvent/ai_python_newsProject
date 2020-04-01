f16afb28a941fda78136b4f0f86260e6255d7bf8
RichardKim <hiddeneventor@gmail.com>
2020-04-01 16:00:42 +0900

    유사성 테스트 코드

--- news_categorizer.py
+++ news_categorizer.py
@@ -3,16 +3,18 @@ import os
 
 def get_file_list(dir_name):
     return os.listdir(dir_name)
-
+# 2. 파일별로 내용 읽기
 def get_conetents(file_list):
     y_class = []
     X_text = []
+    # 0은 야구 1은 축구
     class_dict = {
         1: "0", 2: "0", 3:"0", 4:"0", 5:"1", 6:"1", 7:"1", 8:"1"}
 
     for file_name in file_list:
         try:
-            f = open(file_name, "r",  encoding="cp949")
+            f = open(file_name, "r",  encoding="cp949") # 파일을 하나씩 꺼내와 읽는다 window는 cp949,
+                                                        # mac등은 utf8 인코딩 해줘야 제대로 읽는다.
             category = int(file_name.split(os.sep)[1].split("_")[0])
             y_class.append(class_dict[category])
             X_text.append(f.read())
@@ -23,29 +25,33 @@ def get_conetents(file_list):
     return X_text, y_class
 
 
-
+# 의미없는 문장부호 등은 제거하는 함수
 def get_cleaned_text(text):
     import re
     text = re.sub('\W+','', text.lower() )
     return text
 
-
+# 2. 단어사전 만드는 과정
 def get_corpus_dict(text):
     text = [sentence.split() for sentence in text]
+    
     clenad_words = [get_cleaned_text(word) for words in text for word in words]
 
     from collections import OrderedDict
     corpus_dict = OrderedDict()
+    # 3. set(중복된 단어들은 하나로)으로 묶어주고, enumerate로 인덱스 부여
     for i, v in enumerate(set(clenad_words)):
         corpus_dict[v] = i
     return corpus_dict
 
-
+# 4. 문서별로 Bag of words vector 생성
 def get_count_vector(text, corpus):
     text = [sentence.split() for sentence in text]
     word_number_list = [[corpus[get_cleaned_text(word)] for word in words] for words in text]
+    # 80 x 4024 벡터를 전부 0으로 초기화 해준다.
     X_vector = [[0 for _ in range(len(corpus))] for x in range(len(text))]
 
+    # 벡터에 추가 해주는 작업
     for i, text in enumerate(word_number_list):
         for word_number in text:
             X_vector[i][word_number] += 1
@@ -61,7 +67,7 @@ def get_cosine_similarity(v1,v2):
         sumyy += y*y
         sumxy += x*y
     return sumxy/math.sqrt(sumxx*sumyy)
-
+# 5. 유사성 검토하기
 def get_similarity_score(X_vector, source):
     source_vector = X_vector[source]
     similarity_list = []
@@ -88,19 +94,19 @@ if __name__ == "__main__":
     dir_name = "news_data"
     file_list = get_file_list(dir_name)
     file_list = [os.path.join(dir_name, file_name) for file_name in file_list]
-
+    # 1. 파일 불러오기
     X_text, y_class = get_conetents(file_list)
 
-    corpus = get_corpus_dict(X_text)
+    # 2. 파일을 읽어서 단어사전(Corpus 만들기) && 3. 단어별로 index 만들기
+    corpus = get_corpus_dict(X_text) # 4024개의 단어가 있다...
     print("Number of words : {0}".format(len(corpus)))
+    # 4. 문서별로 Bag of words vector 생성
     X_vector = get_count_vector(X_text, corpus)
     source_number = 10
 
     result = []
-
     for i in range(80):
         source_number = i
-
         similarity_score = get_similarity_score(X_vector, source_number)
         similarity_news = get_top_n_similarity_news(similarity_score, 10)
         accuracy_score = get_accuracy(similarity_news, y_class, source_number)
