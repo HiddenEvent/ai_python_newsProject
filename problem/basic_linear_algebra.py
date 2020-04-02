0d9bd8de297d30d6c3fb60e0009005daf4370b03
RichardKim <hiddeneventor@gmail.com>
2020-04-02 12:37:20 +0900

    선형 대수 문제 완료!!

--- problem/basic_linear_algebra.py
+++ problem/basic_linear_algebra.py
@@ -88,33 +88,73 @@ def matrix_addition(*matrix_variables):
 def matrix_subtraction(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    result = [[element[0]*2 - sum(element) for element in zip(*matrix)] for matrix in zip(*matrix_variables)]
+    return result
 
 # 실행결과
-matrix_x = [[2, 2], [2, 2]]
-matrix_y = [[2, 5], [2, 1]]
-matrix_z = [[2, 4], [5, 3]]
+# matrix_x = [[2, 2], [2, 2]]
+# matrix_y = [[2, 5], [2, 1]]
+# matrix_z = [[2, 4], [5, 3]]
+#
+# print (matrix_subtraction(matrix_x, matrix_y)) # Expected value: [[0, -3], [0, 1]]
+# print (matrix_subtraction(matrix_x, matrix_y, matrix_z)) # Expected value: [[-2, -7], [-5, -2]]
 
-print (matrix_subtraction(matrix_x, matrix_y)) # Expected value: [[0, -3], [0, 1]]
-print (matrix_subtraction(matrix_x, matrix_y, matrix_z)) # Expected value: [[-2, -7], [-5, -2]]
-
-
+# TODO Problem #9 - matrix_transpose (one line code available)
 def matrix_transpose(matrix_variable):
-    return None
+    result = [[i for i in matrix] for matrix in zip(*matrix_variable)]
+    print(result)
+    return result
 
+# 실행결과
+# matrix_w = [[2, 5], [1, 1], [2, 2]]
+# matrix_transpose(matrix_w)
 
+# TODO Problem #10 - scalar_matrix_product (one line code available)
 def scalar_matrix_product(alpha, matrix_variable):
-    return None
+    result = [scalar_vector_product(alpha, matrix) for matrix in matrix_variable]
+    return result
 
+# 실행결과
+# matrix_x = [[2, 2], [2, 2], [2, 2]]
+# matrix_y = [[2, 5], [2, 1]]
+# matrix_z = [[2, 4], [5, 3]]
+# matrix_w = [[2, 5], [1, 1], [2, 2]]
+#
+# print(scalar_matrix_product(3, matrix_x)) #Expected value: [[6, 6], [6, 6], [6, 6]]
+# print(scalar_matrix_product(2, matrix_y)) #Expected value: [[4, 10], [4, 2]]
+# print(scalar_matrix_product(4, matrix_z)) #Expected value: [[8, 16], [20, 12]]
+# print(scalar_matrix_product(3, matrix_w)) #Expected value: [[6, 15], [3, 3], [6, 6]]
 
+# TODO Problem #11 - is_product_availability_matrix (one line code available)
 def is_product_availability_matrix(matrix_a, matrix_b):
-    return None
+    type1 = len([column_vector for column_vector in zip(*matrix_a)])
+    type2 = len(matrix_b)
+    result = type1 == type2
+    return result
+# 실행결과
+# matrix_x= [[2, 5], [1, 1]]
+# matrix_y = [[1, 1, 2], [2, 1, 1]]
+# matrix_z = [[2, 4], [5, 3], [1, 3]]
+#
+# print(is_product_availability_matrix(matrix_y, matrix_z)) # Expected value: True
+# print(is_product_availability_matrix(matrix_z, matrix_x)) # Expected value: True
+# # print(is_product_availability_matrix(matrix_z, matrix_w)) # Expected value: False //matrix_w가없습니다
+# print(is_product_availability_matrix(matrix_x, matrix_x)) # Expected value: True
 
-
+# TODO Problem #12 - matrix_product (one line code available)
 def matrix_product(matrix_a, matrix_b):
     if is_product_availability_matrix(matrix_a, matrix_b) == False:
         raise ArithmeticError
-    return None
+    result = [[sum(a*b for a, b in zip(column_b, row_a))for column_b in zip(*matrix_b)]for row_a in matrix_a]
+    return result
 
-
+# 실행결과
+# matrix_x = [[2, 5], [1, 1]]
+# matrix_y = [[1, 1, 2], [2, 1, 1]]
+# matrix_z = [[2, 4], [5, 3], [1, 3]]
+#
+# print(matrix_product(matrix_y, matrix_z)) # Expected value: [[9, 13], [10, 14]]
+# print(matrix_product(matrix_z, matrix_x)) # Expected value: [[8, 14], [13, 28], [5, 8]]
+# print(matrix_product(matrix_x, matrix_x)) # Expected value: [[9, 15], [3, 6]]
+# print(matrix_product(matrix_z, matrix_w)) # Expected value: False
 
